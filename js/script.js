3f4d804d3c4b07f15e1a0fc3137e3fe4e74a6313
KevinPramudyanto <kevinpramudyanto92@gmail.com>
2024-10-09 21:19:52 +0800

    Add global variable

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*-------------------------------- Constants --------------------------------*/
+
+/*-------------------------------- Variables --------------------------------*/
+let row = 4;
+let col = 4;
+let light = false;
+let moves = 0;
+let timerInterval;
+let cells = [];
